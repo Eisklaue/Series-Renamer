fa2c0ea1cee2b6b58c95bb6ff8c73f649d1e3f50
Andreas Fritz <fritz.andreas@outlook.com>
2016-07-19 08:25:36 +0200

    first version of copy progress in cmd prompt

--- Series-Renamer/Series-Renamer/src/CopyFile.cs
+++ Series-Renamer/Series-Renamer/src/CopyFile.cs
@@ -21,7 +21,8 @@ namespace Series_Renamer.src
             createFolder();
             if (copy)
             {
-                copyFile();
+                copyFile(showPercentProgress);
+                cleanUp();
             }
             else
             {
@@ -41,16 +42,44 @@ namespace Series_Renamer.src
         }
 
 
-        private void copyFile()
+        private void copyFile(Action<string, long, long> reportProgress, int blockSizeToRead = 4096)
         {
-            if (!File.Exists(ep.folder + "\\" + ep.episode))
+            string srcPath = ep.fullPath;
+            string dstPath = ep.folder + "\\" + ep.episode + ".mkv";
+
+            if (!File.Exists(srcPath))
             {
-                File.Create(ep.folder + "\\" + ep.episode);
+                Console.WriteLine("{0} not found.. skipping", srcPath);
+                return;
             }
-            else
+
+            FileInfo srcFileInfo = new FileInfo(srcPath);
+            string message = string.Format("Copy:\t\t");
+
+            if (File.Exists(dstPath))
             {
-                File.Create(ep.folder + "\\" + ep.episode + ep.id);
+                dstPath = ep.folder + "\\" + ep.episode + ep.id + ".mkv";
             }
+
+            byte[] buffer = new byte[blockSizeToRead];
+            using (var dstfs = File.OpenWrite(dstPath))
+            {
+                using (var srcfs = File.OpenRead(srcPath))
+                {
+                    int bytesRead, totalBytesRead = 0;
+                    Console.WriteLine("Move file:\t" + srcPath + "\n" + "to:\t\t" + dstPath);
+                    while ((bytesRead = srcfs.Read(buffer,0,buffer.Length - 1)) > 0)
+                    {
+                        dstfs.Write(buffer, 0, bytesRead);
+                        totalBytesRead += bytesRead;
+                        if (reportProgress != null)
+                        {
+                            reportProgress(message, totalBytesRead, srcFileInfo.Length);
+                        }
+                    }
+                }
+            }
+
         }
 
         private void moveFile()
@@ -61,14 +90,29 @@ namespace Series_Renamer.src
 
             if (!File.Exists(dstPath))
             {
-                Console.WriteLine("Move file:\t" + srcPath + "\n" +"to:\t\t" + dstPath);
+                if( new FileInfo(srcPath).Length == 0)
+                {
+                    Console.WriteLine("Cannot copy file:\t" + srcPath + "\tfile is empty (maybe due to unzip)");
+                }
+                else
+                {
+                    Console.WriteLine("Move file:\t" + srcPath + "\n" + "to:\t\t" + dstPath);
 
-                File.Move(srcPath, dstPath);
+                    File.Move(srcPath, dstPath);
+                }
+               
             }
             else
             {
-                Console.WriteLine("Move file:\t" + srcPath + "\n" + "to:\t\t" + ep.folder + "\\" + ep.episode + ep.id + ".mkv");
-                File.Move(srcPath, ep.folder + "\\" + ep.episode + ep.id + ".mkv");
+                if (new FileInfo(srcPath).Length == 0)
+                {
+                    Console.WriteLine("Cannot copy file:\t" + srcPath + "\tfile is empty (maybe due to unzip)");
+                }
+                else
+                {
+                    Console.WriteLine("Move file:\t" + srcPath + "\n" + "to:\t\t" + ep.folder + "\\" + ep.episode + ep.id + ".mkv");
+                    File.Move(srcPath, ep.folder + "\\" + ep.episode + ep.id + ".mkv");
+                }
             }
         }
 
@@ -86,6 +130,16 @@ namespace Series_Renamer.src
                 {
                     Console.WriteLine(e.Message);
                 }
+            }
+        }
+
+        private void showPercentProgress (string message, long processed, long total)
+        {
+            double percent = processed*100.0/total;
+            Console.Write("\r{0}{1}% complete\t\t {2}/{3}", message, Math.Round(percent,1), processed, total);
+            if (processed >= total - 1)
+            {
+                Console.WriteLine(Environment.NewLine);
             }
         }
     }
